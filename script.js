5b9c566f885cb55649f9ddacbce184d27c0be963
Alexandria <alexandriacwhite@comcast.net>
2022-11-07 12:21:38 -0600

    JavaScript works as radio but working on converting to buttons

--- script.js
+++ script.js
@@ -50,14 +50,17 @@ const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
 const d_text = document.getElementById('d_text')
 const submitBtn = document.getElementById('submit')
-
-
+var time = 50
+var timer = document.querySelector(".timer");
 let currentQuiz = 0
 let score = 0
 
+
+
+
 loadQuiz()
 function loadQuiz() {
-
+deselectAnswers()
 
     const currentQuizData = quizData[currentQuiz]
     a_text.innerText = currentQuizData.a
@@ -68,3 +71,49 @@ function loadQuiz() {
     questionEl.innerText = currentQuizData.question
 }
 
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected(){
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct){
+            score++
+        }
+        currentQuiz++
+
+        if(currentQuiz < quizData.length){
+            loadQuiz()
+        } else {
+            quiz.innerHTML =`
+            <h2>You answered ${score}
+            /${quizData.length} questions correctly</h2>
+            <button onclick="location.reload()".Reload</button>
+            `
+        }
+    }
+   })
+
+/*
+   function countdown_start() {
+    time = 50;
+    var gameTimer = setInterval(function(){
+        time--;
+        timer.textContent= "Time: " + time; 
+//If time hits 0, it will take you to the final page
+    if (time === 0){
+        clearInterval(gameTimer);
+        /*final_score();
+    }
+}, 1000);*/
